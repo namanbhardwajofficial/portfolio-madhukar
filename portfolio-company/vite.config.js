d9b3fed61bfedb4ce8c544c6cb3b10d8f8fc146a
Naman Bhardwaj <namanbhardwaj.official@gmail.com>
2025-06-05 22:09:56 +0530

    final fix

--- portfolio-company/vite.config.js
+++ portfolio-company/vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: "./postcss.config.js",
-  },
+  // Remove this CSS block completely
+  // css: {
+  //   postcss: "./postcss.config.js",
+  // },
 });
